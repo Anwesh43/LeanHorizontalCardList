746440bd8cd3a68a0335638b1753d277658f194e
Anwesh43 <anweshthecool0@gmail.com>
2017-03-31 12:57:41 +0530

    Adding horizontalleancardlist module

---
+++ horizontalcardlist/src/main/java/com/anwesome/ui/horizontalcardlist/LeanCard.java
@@ -0,0 +1,11 @@
+package com.anwesome.ui.horizontalcardlist;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by anweshmishra on 31/03/17.
+ */
+public class LeanCard {
+    private Bitmap bitmap;
+    private String title,subtitle;
+}
---
+++ horizontalcardlist/src/main/java/com/anwesome/ui/horizontalcardlist/LeanMenu.java
@@ -0,0 +1,7 @@
+package com.anwesome.ui.horizontalcardlist;
+
+/**
+ * Created by anweshmishra on 31/03/17.
+ */
+public class LeanMenu {
+}
